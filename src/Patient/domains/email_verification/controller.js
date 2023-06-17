9ec782724c48ac622aeaa3529f1bc2ccb99ea658
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2023-06-17 08:05:28 +0100

    UpdatePatientAuth

--- src/Patient/domains/email_verification/controller.js
+++ src/Patient/domains/email_verification/controller.js
@@ -1,5 +1,19 @@
 const User = require("./../user/model");
-const { sendOTP } = require("./../otp/controller");
+const { sendOTP, verifyOTP, deleteOTP } = require("./../otp/controller");
+
+const verifyUserEmail = async ({ email, otp }) => {
+  try {
+    const validOTP = await verifyOTP({ email, otp });
+    if (!validOTP) {
+      throw Error("Invalid code passed. Check your inbox.");
+    }
+
+    await deleteOTP(email);
+    return;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const sendVerificationOTPEmail = async (email) => {
   try {
@@ -22,4 +36,4 @@ const sendVerificationOTPEmail = async (email) => {
   }
 };
 
-module.exports = { sendVerificationOTPEmail };
+module.exports = { sendVerificationOTPEmail, verifyUserEmail };
--- src/Patient/domains/email_verification/routes.js
+++ src/Patient/domains/email_verification/routes.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { sendVerificationOTPEmail } = require("./controller");
+const { sendVerificationOTPEmail, verifyUserEmail } = require("./controller");
+
+router.post("/verify", async (req, res) => {
+  try {
+    let { email, otp } = req.body;
+
+    if (!(email && otp)) throw Error("Empty otp details are not allowed");
+
+    await verifyUserEmail({ email, otp });
+    res.status(200).json({ email, verified: true });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
 // request new verification otp
 router.post("/", async (req, res) => {
