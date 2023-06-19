cfe1cfe51c8be90b3067b470bbd6ba4f7e86bd86
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2023-06-19 07:37:54 +0100

    UpdatePatientAuth

--- src/Patient/domains/user/routes.js
+++ src/Patient/domains/user/routes.js
@@ -47,63 +47,63 @@ router.post("/signup", async (req, res) => {
     dob = dob.trim();
     password = password.trim();
 
-    if (!(name && email && dob && password)) {
-      // throw Error("Empty input fields!");
-      res
-        .status(400)
-        .json({ message: "Empty input fields!", status: "FAILED" });
-    } else if (!/^[a-zA-Z ]*$/.test(name)) {
-      // throw Error("Invalid name entered");
-      res
-        .status(400)
-        .json({ message: "Invalid name entered", status: "FAILED" });
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-      // throw Error("Invalid email entered");
-      res
-        .status(400)
-        .json({ message: "Invalid email entered", status: "FAILED" });
-    } else if (password.length < 8) {
-      // throw Error("Password is too short!");
-      res
-        .status(400)
-        .json({ message: "Password is too short!", status: "FAILED" });
-    } else {
-      // const profile = req.files.profilePicture;
-      // if (
-      //   profile.mimetype == "image/apng" ||
-      //   profile.mimetype == "image/avif" ||
-      //   profile.mimetype == "image/gif" ||
-      //   profile.mimetype == "image/jpeg" ||
-      //   profile.mimetype == "image/png" ||
-      //   profile.mimetype == "image/svg+xml" ||
-      //   profile.mimetype == "image/webp"
-      // ) {
-      //   const upload = await cloudinary.v2.uploader.upload(
-      //     profile.tempFilePath,
-      //     {
-      //       resource_type: "image",
-      //       folder: process.env.patientProfilePictureFolder,
-      //       use_filename: false,
-      //       unique_filename: true,
-      //     }
-      //   );
-      // good credentials, create new user
-      const newUser = await createNewUser({
-        name,
-        email,
-        dob,
-        password,
-        // profilePic: upload.secure_url,
-        // profilePicID: upload.public_id,
-      });
-      res.status(200).json(newUser);
-      // } else {
-      //   // throw Error("Invalid Image File Type");
-      //   res
-      //     .status(400)
-      //     .send({ message: "Invalid Image File Type", status: "FAILED" });
-      // }
-    }
+    // if (!(name && email && dob && password)) {
+    //   // throw Error("Empty input fields!");
+    //   res
+    //     .status(400)
+    //     .json({ message: "Empty input fields!", status: "FAILED" });
+    // } else if (!/^[a-zA-Z ]*$/.test(name)) {
+    //   // throw Error("Invalid name entered");
+    //   res
+    //     .status(400)
+    //     .json({ message: "Invalid name entered", status: "FAILED" });
+    // } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+    //   // throw Error("Invalid email entered");
+    //   res
+    //     .status(400)
+    //     .json({ message: "Invalid email entered", status: "FAILED" });
+    // } else if (password.length < 8) {
+    //   // throw Error("Password is too short!");
+    //   res
+    //     .status(400)
+    //     .json({ message: "Password is too short!", status: "FAILED" });
+    // } else {
+    // const profile = req.files.profilePicture;
+    // if (
+    //   profile.mimetype == "image/apng" ||
+    //   profile.mimetype == "image/avif" ||
+    //   profile.mimetype == "image/gif" ||
+    //   profile.mimetype == "image/jpeg" ||
+    //   profile.mimetype == "image/png" ||
+    //   profile.mimetype == "image/svg+xml" ||
+    //   profile.mimetype == "image/webp"
+    // ) {
+    //   const upload = await cloudinary.v2.uploader.upload(
+    //     profile.tempFilePath,
+    //     {
+    //       resource_type: "image",
+    //       folder: process.env.patientProfilePictureFolder,
+    //       use_filename: false,
+    //       unique_filename: true,
+    //     }
+    //   );
+    // good credentials, create new user
+    const newUser = await createNewUser({
+      name,
+      email,
+      dob,
+      password,
+      // profilePic: upload.secure_url,
+      // profilePicID: upload.public_id,
+    });
+    res.status(200).json(newUser);
+    // } else {
+    //   // throw Error("Invalid Image File Type");
+    //   res
+    //     .status(400)
+    //     .send({ message: "Invalid Image File Type", status: "FAILED" });
+    // }
+    // }
   } catch (error) {
     res.status(400).send(error.message);
   }
