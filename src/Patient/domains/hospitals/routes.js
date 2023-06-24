0a612d817197b59e4677ea896caf512c23ffc41a
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2023-06-24 02:36:26 +0100

    UpdatePatientSide

--- src/Patient/domains/hospitals/routes.js
+++ src/Patient/domains/hospitals/routes.js
@@ -2,11 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const hospitalsMod = require("./../../../WebPortal/models/HospitalSide/Profile/profile");
+const doctorMod = require('./../../../WebPortal/models/HospitalSide/Doctor/doctor')
 
 router.get('/', async(req, res) => {
 try {
     const hospitals = await hospitalsMod.find().limit(3)
     res.status(200).json(hospitals)
+} catch (error) {
+    res.status(400).send(error.message);
+}
+})
+
+router.get('/doctors/:id', async(req, res) => {
+try {
+    const doctors = await doctorMod.find({hospital: req.params.id})
+    res.status(200).json(doctors)
 } catch (error) {
     res.status(400).send(error.message);
 }
