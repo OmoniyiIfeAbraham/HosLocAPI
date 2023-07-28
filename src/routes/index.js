cb9dfe64a9b4264436469263c7fa9c04c02d0720
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2023-07-28 08:38:06 +0100

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,19 +15,4 @@ router.use("/forgot_password", ForgotPasswordRoutes);
 router.use("/hospitals", hospitalsRoutes);
 router.use("/doctor", doctorRoutes);
 
-router.get("/none", (req, res) => {
-  const dos = false;
-  if (dos == true) {
-    res.status(400).send({
-      message: "HosLoc is Undergoing Update. Please check back in a few days.",
-      status: "FAILED",
-    });
-  } else {
-    res.status(400).send({
-      message: "go",
-      status: "FAILED",
-    });
-  }
-});
-
 module.exports = router;
